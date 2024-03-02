8cb209b8544d60cf9bff9f668bae7f96ffb9c75b
Sebastian Martinez <sebastian.martinez@lever.co>
2024-03-01 22:32:09 -0500

    child

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-import http from "http"
-import fs from "fs"
-import os from 'os'
+import childProcess from 'child_process';
 
+const execProcess = (command) => {
+  childProcess.exec(command, (error, stdout, stderr) => {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
 
-const port = 4000;
-
-const server = http.createServer((req, res) => {
-  fs.readFile('page.html', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    console.log(data.toString());
-    console.log(os.arch());
-    console.log(os.constants);
-    console.log(os.cpus);
-    console.log(os.endianness);
-    console.log(os.EOL);
-    console.log(os.freemem);
-    console.log(os.hostname);
-    return res.end();
+    if (error !== null) {
+      console.log(`error: ${error}`);
+    }
   });
-});
+}
 
-server.listen(port, () => {
-  console.log(`Server running 🚀 at http://localhost:${port}/`);
-});
+execProcess('node -v');
+
+const spawnProcess = (command, args) => {
+  const process = childProcess.spawn(command, args);
+  let fullData = '';
+  let dataChunks = 0;
+
+  process.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  process.stdout.on('data', (data) => {
+    fullData += data;
+    dataChunks += 1;
+    console.log(`stdout: ${data}`);
+  });
+
+  // end of data stream, there we can output the data
+  process.stdout.on('end', () => {
+    console.log(`end: ${fullData}`);
+    console.log(`chunks: ${dataChunks}`);
+  });
+
+  // event when process is finished
+  // there we can get to know with what code it was ended (0 - success, 1 - error)
+  process.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+spawnProcess('node', ['-v']);
