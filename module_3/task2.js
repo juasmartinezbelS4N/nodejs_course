5094ec27ff51023ae92a9f9148fc6ffeda462583
Sebastian Martinez <sebastian.martinez@lever.co>
2024-03-11 10:04:11 -0500

    Task 2 implemented

--- module_3/task2.js
+++ module_3/task2.js
@@ -1,24 +1,38 @@
-import EventEmitter from "./eventEmitter.js";
+import fs from "fs"
+import EventEmitter from "./eventEmitter.js"
 
 class WithTime extends EventEmitter {
   execute(asyncFunc, ...args) {
-      // emit event start, end, data received
-      // call asyncFunc with args specified
-      // compute the time it takes to execute asyncFunc
+    console.time("execute(asyncFunc, ...args)")
+    this.emit("begin")
+    const cb = (err, data) => {
+      if (err) {
+        throw new Error(err)
+      }
+      this.emit("data", data)
+      this.emit("end")
+      console.timeEnd("execute(asyncFunc, ...args)")
+    }
+    asyncFunc(...args, cb)
   }
 }
 
 const fetchFromUrl = (url, cb) => {
-  // fetch from https://jsonplaceholder.typicode.com/posts/1
-  // transform to JSON
+  fetch(url)
+    .then((res) => res.json())
+    .then((parsedRes) => {
+      cb(null, parsedRes);
+    }).catch((err) => {
+      cb(err);
+    })
 }
 
+const withTime = new WithTime()
 
-const withTime = new WithTime();
+withTime.on("begin", () => console.log("About to execute"))
+withTime.on("data", (data) => console.log("Data:", data))
+withTime.on("end", () => console.log("Done with execute"))
 
-withTime.on('begin', () => console.log('About to execute'));
-withTime.on('end', () => console.log('Done with execute'));
+withTime.execute(fetchFromUrl, "https://jsonplaceholder.typicode.com/posts/1")
 
-withTime.execute(fetchFromUrl, 'https://jsonplaceholder.typicode.com/posts/1');
-
-console.log(withTime.rawListeners("end"));
+console.log(withTime.rawListeners("end"))
