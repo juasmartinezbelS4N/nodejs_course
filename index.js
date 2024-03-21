47d8145f475056c6e7ab17247fa0fbda9283b44b
Sebastian Martinez <sebastian.martinez@lever.co>
2024-03-21 11:03:54 -0500

    MODULE-5 Adding methods for Users

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-export default function getRandomNumber() {
-  console.log(Math.floor(Math.random() * 1000) + 1)
+import http from "http"
+import { usersAPI } from "./src/api/users.js"
+import { hobbiesAPI } from "./src/api/hobbies.js"
+import { returnInvalidPageError } from "./src/api/utils.js"
+
+const API_URL = "/api/users"
+
+const validateURL = (url) => {
+  return !url.includes(API_URL)
 }
 
-getRandomNumber()
+const server = http.createServer((req, res) => {
+  const { url } = req
+  if (validateURL(url)) {
+    returnInvalidPageError(res)
+    return
+  }
+  if (url === API_URL) {
+    usersAPI(req, res)
+  } else {
+    const parsedUrl = url.split("/")
+    const baseUrl = `/${parsedUrl[1]}/${parsedUrl[2]}`
+    if (baseUrl !== API_URL) {
+      returnInvalidPageError(res)
+      return
+    }
+
+    if (parsedUrl.length === 4) {
+      usersAPI(req, res)
+    } else if (parsedUrl.length === 5 && parsedUrl[4] === "hobbies") {
+      hobbiesAPI(req, res)
+    } else {
+      returnInvalidPageError(res)
+    }
+  }
+})
+
+server.listen(8000, () => {
+  console.log("Server is running on port 8000")
+})
---
+++ src/api/users.js
@@ -0,0 +1,77 @@
+import {
+  returnInvalidMethodError,
+  parseRequestBody,
+  returnServerError,
+  createUser,
+  parseData,
+  returnInvalidPageError,
+  returnWrongDataError,
+} from "./utils.js"
+import database from "./database.js"
+
+export const usersAPI = (req, res) => {
+  const { url, method, headers } = req
+  switch (method) {
+    case "POST":
+      if (url.split("/").length === 3) postUsers(req, res)
+      else returnInvalidMethodError(res)
+      return
+    case "GET":
+      if (url.split("/").length === 3) getUsers(req, res)
+      else returnInvalidMethodError(res)
+      return
+    case "DELETE":
+      deleteUsers(req, res)
+      return
+    default:
+      returnInvalidMethodError(res)
+      return
+  }
+}
+
+export const postUsers = async (req, res) => {
+  const reqBody = await parseRequestBody(req)
+  if (!reqBody) {
+    returnServerError(res)
+    return
+  }
+  if (!reqBody.name || !reqBody.email) {
+    returnWrongDataError(res)
+    return
+  }
+  res.writeHead(201, { "Content-Type": "application/json" })
+  const user = createUser(reqBody)
+  res.end(user)
+}
+
+export const getUsers = (_req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Cache-Control": "max-age=3600",
+  })
+  res.end(parseData(database.users))
+}
+
+export const deleteUsers = (req, res) => {
+  const userId = req.url.split("/")[3]
+  if (!userId) {
+    returnInvalidMethodError(res)
+    return
+  }
+  const newUsers = []
+  const { users } = database
+  users.forEach((ogUser) => {
+    if (ogUser.user.id !== userId) newUsers.push(ogUser)
+  })
+
+  if (users.length === newUsers.length) {
+    returnInvalidPageError(
+      res,
+      parseData(null, `User with id ${userId} doesn't exist`)
+    )
+    return
+  }
+  database.users = newUsers
+  res.writeHead(200, { "Content-Type": "application/json" })
+  res.end(parseData({ success: true }))
+}
